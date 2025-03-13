4fd10de615a69705994fca8cf092d66bbe85ebb9
Crolev <crolev.eth@gmail.com>
2025-03-13 22:57:09 +0100

    add bsc to the reformat script

--- reformat.py
+++ reformat.py
@@ -1,17 +1,32 @@
 import json
-import argparse
+
+FOLDER = "data/7_Mar-13-2025_103.5k"
+INPUT_FILE = f"{FOLDER}/mpx_final_snapshot_ftm_105217394_bsc_46881358.json"
+FTM_RAW_FILE = f"{FOLDER}/mpx_ftm_snapshot_105217394.json"
+BNB_RAW_FILE = f"{FOLDER}/mpx_bnb_snapshot_46881358.json"
+OUTPUT_FILE = f"{FOLDER}/mpx_final_snapshot.json"
 
 
-def reformat_json(input_file, raw_file, output_file):
+def reformat_json(input_file, ftm_raw_file, bnb_raw_file, output_file):
     with open(input_file, 'r') as f:
         input_data = json.load(f)
 
-    with open(raw_file, 'r') as f:
-        raw_data = json.load(f)
-    raw_dict = {}
-    for item in raw_data:
+    with open(ftm_raw_file, 'r') as f:
+        ftm_raw_data = json.load(f)
+    ftm_dict = {}
+    for item in ftm_raw_data:
         addr = item.get("address", "").lower()
-        raw_dict[addr] = {
+        ftm_dict[addr] = {
+            "amount": item.get("amount"),
+            "amountLp": item.get("amountLp")
+        }
+
+    with open(bnb_raw_file, 'r') as f:
+        bnb_raw_data = json.load(f)
+    bnb_dict = {}
+    for item in bnb_raw_data:
+        addr = item.get("address", "").lower()
+        bnb_dict[addr] = {
             "amount": item.get("amount"),
             "amountLp": item.get("amountLp")
         }
@@ -20,22 +35,21 @@ def reformat_json(input_file, raw_file, output_file):
         amount_usdc = float(item.get("amount", 0)) / 1e6
         item["amountUsdc"] = amount_usdc
         item.pop("amount", None)
-
         address_lower = item.get("address", "").lower()
-        if address_lower in raw_dict:
-            raw_info = raw_dict[address_lower]
-            item["amountMpx"] = raw_info["amount"]
-            item["amountLp"] = raw_info["amountLp"]
+
+        if address_lower in ftm_dict:
+            ftm_info = ftm_dict[address_lower]
+            item["amountMpxOnFtm"] = ftm_info["amount"]
+            item["amountLpOnFtm"] = ftm_info["amountLp"]
+
+        if address_lower in bnb_dict:
+            bnb_info = bnb_dict[address_lower]
+            item["amountMpxOnBsc"] = bnb_info["amount"]
+            item["amountLpOnBsc"] = bnb_info["amountLp"]
 
     with open(output_file, 'w') as f:
         json.dump(input_data, f, indent=2)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Reformat JSON file.')
-    parser.add_argument('input_file', type=str, help='Path to the input JSON file.')
-    parser.add_argument('raw_file', type=str, help='Path to the raw JSON file for enrichment.')
-    parser.add_argument('output_file', type=str, help='Path to the output JSON file.')
-
-    args = parser.parse_args()
-    reformat_json(args.input_file, args.raw_file, args.output_file)
+    reformat_json(INPUT_FILE, FTM_RAW_FILE, BNB_RAW_FILE, OUTPUT_FILE)
